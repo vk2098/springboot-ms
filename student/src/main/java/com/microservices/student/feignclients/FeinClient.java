2028675733a2549da16a9022938a414644b6b02c
vk2098 <kapavivek98@gmail.com>
2024-11-02 21:33:34 -0500

    updated Feign Client name

--- student/src/main/java/com/microservices/student/feignclients/FeinClient.java
+++ student/src/main/java/com/microservices/student/feignclients/FeinClient.java
@@ -6,7 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "${address.service.url}",name = "address-feign-client", path = "/api/address")
+//@FeignClient(url = "${address.service.url}",name = "address-feign-client", path = "/api/address")
+@FeignClient(name = "address", path = "/api/address")
+
 public interface FeinClient {
     @GetMapping("/getById/{id}")
     public AddressResponse getById(@PathVariable long id);
