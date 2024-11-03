480234a8b20dfcfd6f86de8c04f0aeecb588801d
vk2098 <kapavivek98@gmail.com>
2024-11-03 14:59:35 -0600

    ApiGateway does the load balancing since feign client is directed through it

--- student/src/main/java/com/microservices/student/loadbalancer/LoadBalancer.java
+++ student/src/main/java/com/microservices/student/loadbalancer/LoadBalancer.java
@@ -1,20 +1,20 @@
-package com.microservices.student.loadbalancer;
-
-import com.microservices.student.response.AddressResponse;
-import feign.Feign;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-
-@LoadBalancerClient(value = "ADDRESS-SERVICE")
-public class LoadBalancer {
-    @Bean
-    @LoadBalanced
-    public Feign.Builder feignBuilder(){
-        return  Feign.builder();
-    }
-
-}
+//package com.microservices.student.loadbalancer;
+//
+//import com.microservices.student.response.AddressResponse;
+//import feign.Feign;
+//import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+//import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+//import org.springframework.cloud.openfeign.FeignClient;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//
+//@LoadBalancerClient(value = "ADDRESS-SERVICE")
+//public class LoadBalancer {
+//    @Bean
+//    @LoadBalanced
+//    public Feign.Builder feignBuilder(){
+//        return  Feign.builder();
+//    }
+//
+//}
