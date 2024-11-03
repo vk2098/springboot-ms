42034e59e767a4fc55ba94d54757a9e13bdb1ac5
vk2098 <kapavivek98@gmail.com>
2024-11-02 21:22:15 -0500

    added eureka server and client

--- student/src/main/java/com/microservices/student/StudentApplication.java
+++ student/src/main/java/com/microservices/student/StudentApplication.java
@@ -3,12 +3,14 @@ package com.microservices.student;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class StudentApplication {
 
 
